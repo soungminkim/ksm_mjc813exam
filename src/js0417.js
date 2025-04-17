236e2ba8ca6389b39062adcc9950c81ba0052299
soungminkim <21soungmin12@naver.com>
2025-04-17 18:00:20 +0900

    add 문제 27 추가버전

---
+++ src/js0417.js
@@ -0,0 +1,38 @@
+
+class Exam27 {
+    maps = new Map();
+    inputFunc() {
+        let names = prompt("이름을 공백으로 구분하여 입력하세요.");
+        let score = prompt("점수를 공백으로 구분하여 입력하세요.");
+        let arrName = names.split(" ");
+        let arrScore = score.split(" ");
+        if (arrName.length != arrScore.length) {
+            alert("두개 배열의 길이가 다릅니다. 다시 시작하세요.");
+            return false;   // 함수가 false 를 리턴한다.
+        }
+        for (let i = 0; i < arrScore.length; i++) {
+            this.maps.set(arrName[i], Number(arrScore[i])); // Number 는 문자열을 숫자로 변경
+            console.log(this.maps);
+        }
+        return true;
+    }
+
+    outputFunc() {
+        let strOutput = "{";
+        for( let item of this.maps ) {
+            let j = 0;
+            console.log(item);
+            strOutput += `'${item[j]}': ${item[j+1]}, `;
+            j++;
+        }
+        strOutput = strOutput.substring(0 , strOutput.length - 2);
+        // 문자열의 길이에서 -2 시킨 길이의 문자열로 다시 저장. "abcd" => "ab"
+        strOutput += "}";
+        console.log(strOutput);
+    }
+}
+
+let exam = new Exam27();
+if ( exam.inputFunc() ) {   // inputFunc() 를 실행하고 리턴값이 true 이면
+    exam.outputFunc();  // outputFunc() 를 실행한다.
+}
