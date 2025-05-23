17a2be977243db28691b6a4d2699e04b6f6819c3
soungminkim <21soungmin12@naver.com>
2025-05-23 18:24:38 +0900

    main으로 변경

--- mavenJUnit/src/main/java/com/mjc/studyjava/Market.java
+++ mavenJUnit/src/main/java/com/mjc/studyjava/Market.java
@@ -1,18 +1,15 @@
 package com.mjc.studyjava;
 
-
-import com.mjc813.mjc_library.IMarketInfo;
 import com.mjc813.mjc_library.MjcPoint;
 
 public class Market {
     public static void main(String[] args) {
-        MjcPoint mjcPoint = new MjcPoint();
-        IMarketInfo marketInfo = new MarketInfo();
+        MjcPoint mp = new MjcPoint();
+        mp.getMarketPoint();
+        mp.addPoint(null, 10);
+        mp.addPoint(new MarketInfo(), 50);
 
-        mjcPoint.addPoint(marketInfo, 10);
-
-        mjcPoint.subPoint(marketInfo,10);
-
-        mjcPoint.subPoint(marketInfo, 25);
+        mp.subPoint(null, 20);
+        mp.subPoint(new MarketInfo(), 20);
     }
 }
