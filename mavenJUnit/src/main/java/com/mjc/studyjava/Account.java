9f77e2975ba0354366a10de5cf371c768e67bb78
soungminkim <21soungmin12@naver.com>
2025-05-26 20:58:23 +0900

    add

--- mavenJUnit/src/main/java/com/mjc/studyjava/Account.java
+++ mavenJUnit/src/main/java/com/mjc/studyjava/Account.java
@@ -1,31 +1,29 @@
 package com.mjc.studyjava;
 
 public class Account {
-//    Account : 속성{ 계좌번호(accountNumber), 계좌주(accountName), 금액(money) }
-//    동작{ 생성자(계좌번호,계좌주), addMoney(금액), subMoney(금액) }
-    private String accoutNumber;
+    //    Account : 속성{ 계좌번호(accountNumber), 계좌주(accountName), 금액(money) }
+    //    동작{ 생성자(계좌번호,계좌주), addMoney(금액), subMoney(금액) }
+    private String accountNumber;
     private String accountName;
-    private int balance;
+    private int money;
 
     /**
      * Account 비기본 생성자 (계좌번호, 계좌주)
+     *
      * @param accountNumber
      * @param accountName
      */
     public Account(String accountNumber, String accountName) {
         this(accountNumber, accountName, 0);
     }
-
     /**
      * Account 비기본 생성자 (계좌번호, 계좌주, 초기금액)
      * @param accountNumber
-     * @param accountName
-     * @param balance
      */
-    public Account(String accountNumber, String accountName, int balance) {
-        this.accoutNumber = accountNumber;
+    public Account(String accountNumber, String accountName, int money) {
+        this.accountNumber = accountNumber;
         this.accountName = accountName;
-        this.balance = balance;
+        this.money = money;
     }
 
     /**
@@ -33,12 +31,14 @@ public class Account {
      * @param m
      */
     void addMoney(int m) {
+        this.money += m;
     }
     /**
      * m 을 금액에서 뺀다.
      * @param m
      */
     void subMoney(int m) {
+        this.money -= m;
     }
 
     /**
@@ -46,14 +46,14 @@ public class Account {
      * @return
      */
     public String getAccountNumber() {
-        return "";
+        return this.accountNumber;
     }
     /**
      * 계좌번호 정보를 저장한다.
      * @param accoutNumber
      */
-    public void setAccoutNumber(String accoutNumber) {
-
+    public void setAccountNumber(String accoutNumber) {
+        this.accountNumber = accoutNumber;
     }
 
     /**
@@ -61,14 +61,14 @@ public class Account {
      * @return
      */
     public String getAccountName() {
-        return "";
+        return this.accountName;
     }
     /**
      * 계좌주 정보를 저장한다.
      * @param accountName
      */
     public void setAccountName(String accountName) {
-
+        this.accountName = accountName;
     }
 
     /**
@@ -76,14 +76,14 @@ public class Account {
      * @return
      */
     public int getBalance() {
-        return 0;
+        return money;
     }
 
     /**
      * 금액 정보를 저장한다.
-     * @param balance
+     * @param money
      */
-    public void setBalance(int balance) {
-
+    public void setBalance(int money) {
+        this.money = money;
     }
 }
